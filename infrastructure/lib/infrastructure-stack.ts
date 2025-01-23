8a8010d5a77c9f4b27ee0b629be249f65b170e3a
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2025-01-23 12:04:59 +0200

    chore fix deploy #3

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,8 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
-
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,7 +20,10 @@ export class InfrastructureStack extends cdk.Stack {
     // CloudFront
     const distribution = new cloudfront.Distribution(this, 'MathTestDistribution', {
       defaultBehavior: {
-        origin: new cloudfrontOrigins.S3Origin(mathTestBucket),
+        origin: new origins.OriginGroup({
+          primaryOrigin: origins.S3BucketOrigin.withOriginAccessControl(mathTestBucket),
+          fallbackOrigin: origins.S3BucketOrigin.withOriginAccessControl(mathTestBucket),
+        }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
       defaultRootObject: 'index.html',
