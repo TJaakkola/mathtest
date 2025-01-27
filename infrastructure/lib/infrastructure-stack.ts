217bd14560c38306346bcc256fc898a79c232d8e
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2025-01-27 13:09:09 +0200

    fix cdn to s3 connection by removing websiteIndexDocument parameter from bucket which made cdn unable to connect to S3 bucket via rest api

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -3,44 +3,67 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // S3 bucket for static website
-    const mathTestBucket = new s3.Bucket(this, 'math-test-bucket', {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'WebsiteBucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      publicReadAccess: false,
     });
 
-    // CloudFront
-    const distribution = new cloudfront.Distribution(this, 'MathTestDistribution', {
+    // Create Origin Access Identity for CloudFront
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
+    // Grant read permissions to CloudFront
+    bucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [bucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+
+    // Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
-        origin: new origins.OriginGroup({
-          primaryOrigin: origins.S3BucketOrigin.withOriginAccessControl(mathTestBucket),
-          fallbackOrigin: origins.S3BucketOrigin.withOriginAccessControl(mathTestBucket),
+        origin: new origins.S3Origin(bucket, {
+          originAccessIdentity: originAccessIdentity
         }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
       defaultRootObject: 'index.html',
+      // Add error responses for SPA routing
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        }
+      ]
     });
 
     // S3-tiedostojen deploy
-    // new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
-    //   sources: [s3Deployment.Source.asset('../path-to-your-vite-build-folder')],
-    //   destinationBucket: mathTestBucket,
-    //   distribution,
-    //   distributionPaths: ['/*'],
-    // });
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('../dist')],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
 
     new cdk.CfnOutput(this, 'BucketName', {
-      value: mathTestBucket.bucketName,
+      value: bucket.bucketName,
     });
-    new cdk.CfnOutput(this, 'CloudFrontURL', {
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
       value: distribution.distributionDomainName,
     });
 
